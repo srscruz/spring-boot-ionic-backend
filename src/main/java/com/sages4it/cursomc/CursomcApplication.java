41db07d431a39cbe350785e0965892d93981439e
Sergio Cruz <sergio.cruz@sages4it.com>
2020-03-18 03:15:56 -0300

    Pedido, estado pagamento e pagamento

--- src/main/java/com/sages4it/cursomc/CursomcApplication.java
+++ src/main/java/com/sages4it/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.sages4it.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.sages4it.cursomc.domain.Cidade;
 import com.sages4it.cursomc.domain.Cliente;
 import com.sages4it.cursomc.domain.Endereco;
 import com.sages4it.cursomc.domain.Estado;
+import com.sages4it.cursomc.domain.Pagamento;
+import com.sages4it.cursomc.domain.PagamentoComBoleto;
+import com.sages4it.cursomc.domain.PagamentoComCartao;
+import com.sages4it.cursomc.domain.Pedido;
 import com.sages4it.cursomc.domain.Produto;
+import com.sages4it.cursomc.domain.enums.EstadoPagamento;
 import com.sages4it.cursomc.domain.enums.TipoCliente;
 import com.sages4it.cursomc.repositories.CategoriaRepository;
 import com.sages4it.cursomc.repositories.CidadeRepository;
 import com.sages4it.cursomc.repositories.ClienteRepository;
 import com.sages4it.cursomc.repositories.EnderecoRepository;
 import com.sages4it.cursomc.repositories.EstadoRepository;
+import com.sages4it.cursomc.repositories.PagamentoRepository;
+import com.sages4it.cursomc.repositories.PedidoRepository;
 import com.sages4it.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -40,6 +48,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -71,20 +83,35 @@ public class CursomcApplication implements CommandLineRunner {
 
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est1.getCidades().addAll(Arrays.asList(c2, c3));
-		
-		estadoRepository.saveAll(Arrays.asList(est1,est2));
-		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
-		
-		Cliente cli1 = new Cliente(null,"Sergio Cruz", "sergio.cruz@sages4it.com", "18419691844", TipoCliente.PESSOAFISICA);
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		Cliente cli1 = new Cliente(null, "Sergio Cruz", "sergio.cruz@sages4it.com", "18419691844",
+				TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("55394219", "971719217"));
-		
+
 		Endereco e1 = new Endereco(null, "Av. Maestro Vilas Lobos", "805", "A", "Tucuruvi", "02260060", cli1, c2);
 		Endereco e2 = new Endereco(null, "Rua Estela", "515", "Bloco F, Conj. 182", "Paraiso", "04091000", cli1, c2);
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 
-		
 		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1,e2));	
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2019 10:22"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("22/02/2020 09:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, null,
+				sdf.parse("23/03/2020 00:00"));
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
--- src/main/java/com/sages4it/cursomc/domain/Cliente.java
+++ src/main/java/com/sages4it/cursomc/domain/Cliente.java
@@ -35,6 +35,10 @@ public class Cliente implements Serializable{
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
+
 	public Cliente() {
 		
 	}
@@ -102,6 +106,39 @@ public class Cliente implements Serializable{
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 
