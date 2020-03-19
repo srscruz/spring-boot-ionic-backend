255a83f296a84ac63b80ca5ab3277588bbdfca67
Sergio Cruz <sergio.cruz@sages4it.com>
2020-03-19 12:10:57 -0300

    Atualizaçao da Categoria com metodo PUT

--- src/main/java/com/sages4it/cursomc/services/CategoriaService.java
+++ src/main/java/com/sages4it/cursomc/services/CategoriaService.java
@@ -18,13 +18,18 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: "+ id + " Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + " Tipo: " + Categoria.class.getName()));
 
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
+	}
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+		
 	}
 
 }
