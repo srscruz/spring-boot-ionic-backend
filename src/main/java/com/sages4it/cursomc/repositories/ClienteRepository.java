8ac33b63e4040a4b628c906eef69515c9985c462
Sergio Cruz <sergio.cruz@sages4it.com>
2020-03-21 02:05:01 -0300

    validaçao de cpf, cnpj e email se o dado já existe na base de dados

--- src/main/java/com/sages4it/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/sages4it/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,19 @@ package com.sages4it.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sages4it.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	@org.springframework.transaction.annotation.Transactional(readOnly =  true)
+	Cliente findByEmail(String email);
+	
+	@Transactional(readOnly = true)
+	Cliente cpfOuCnpj(String cpfOuCnpj);
+	
+	
 
 }
--- src/main/java/com/sages4it/cursomc/services/validator/ClienteInsertValidator.java
+++ src/main/java/com/sages4it/cursomc/services/validator/ClienteInsertValidator.java
@@ -6,8 +6,12 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sages4it.cursomc.domain.Cliente;
 import com.sages4it.cursomc.domain.enums.TipoCliente;
 import com.sages4it.cursomc.dto.ClienteNewDTO;
+import com.sages4it.cursomc.repositories.ClienteRepository;
 import com.sages4it.cursomc.resources.exceptions.FieldMessage;
 import com.sages4it.cursomc.services.validator.util.BR;
 
@@ -17,14 +21,33 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 	}
 
+	@Autowired
+
+	private ClienteRepository repo;
+
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		Cliente aux1 = repo.cpfOuCnpj(objDto.getCpfOuCnpj());
+		
 		List<FieldMessage> list = new ArrayList<>();
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("CpfOuCnpj", "CPF invalido !"));
-		}
+		} 
+			
+		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("CpfOuCnpj", "CNPJ invalido !"));
+		}
+
+		if (aux1 != null) {
+			list.add(new FieldMessage("CpfOuCnpj", "CPF ou CNPJ já existe na base"));
+		}
+
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existe na base"));
+
 		}
 
 		for (FieldMessage e : list) {
@@ -34,4 +57,5 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		return list.isEmpty();
 	}
+
 }
