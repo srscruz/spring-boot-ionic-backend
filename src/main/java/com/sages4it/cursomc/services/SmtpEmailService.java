93e5b43e957ee6d5b76991ae06e28925865c3ced
Sergio Cruz <sergio.cruz@sages4it.com>
2020-03-24 18:39:12 -0300

    implementaçao do html para envio de email

--- src/main/java/com/sages4it/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/sages4it/cursomc/services/SmtpEmailService.java
@@ -1,15 +1,20 @@
 package com.sages4it.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 
 	@Autowired
 	private MailSender mailSender;
+	@Autowired
+	private JavaMailSender javaMailSender;
 	
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 	@Override
@@ -17,6 +22,13 @@ public class SmtpEmailService extends AbstractEmailService {
 		LOG.info("Enviando emial");
 		mailSender.send(msg);
 		LOG.info("Email enviado");
+	}
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando emial");
+		javaMailSender.send(msg);
+		LOG.info("Email enviado");	
+		
 	}
 
 }
