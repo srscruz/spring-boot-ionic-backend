550e6888b7196b26845051729f4c804abc37721d
Sergio Cruz <sergio.cruz@sages4it.com>
2020-03-24 16:54:11 -0300

    Implentaçao do serviço de en envio de email, utilizando google

--- src/main/java/com/sages4it/cursomc/config/DevConfig.java
+++ src/main/java/com/sages4it/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.sages4it.cursomc.services.DBService;
+import com.sages4it.cursomc.services.EmailService;
+import com.sages4it.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,4 +32,8 @@ public class DevConfig {
 		return true;
 	}
 
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
 }
