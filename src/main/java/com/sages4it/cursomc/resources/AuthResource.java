69fc3a40f181bdd09917ffecd18149d32170bd7f
Sergio Cruz <sergio.cruz@sages4it.com>
2020-04-04 03:45:17 -0300

    implementaçao do esqueci a senha

--- src/main/java/com/sages4it/cursomc/resources/AuthResource.java
+++ src/main/java/com/sages4it/cursomc/resources/AuthResource.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sages4it.cursomc.dto.EmailDTO;
 import com.sages4it.cursomc.security.JWTUtil;
 import com.sages4it.cursomc.security.UserSS;
+import com.sages4it.cursomc.services.AuthService;
 import com.sages4it.cursomc.services.UserService;
 @RestController
 @RequestMapping(value = "/auth")
@@ -20,8 +22,8 @@ public class AuthResource {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
-	//@Autowired
-	//private AuthService service;
+	@Autowired
+	private AuthService service;
 	
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
@@ -32,9 +34,9 @@ public class AuthResource {
 		return ResponseEntity.noContent().build();
 	}
 	
-//	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
-//	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
-//		service.sendNewPassword(objDto.getEmail());
-//		return ResponseEntity.noContent().build();
-//	}
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
+		return ResponseEntity.noContent().build();
+	}
 }
